54fe27fa581f9fadc1cbb2c6c36debb79f83d445
RakshaFutarmal <raksha.futarmal@tntra.io>
2026-09-23 22:25:33 +0530

    [ADD] LLM config added

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -10,7 +10,7 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from app.core.config import settings
+from app.db.session import settings
 from app.core.database import Base
 
 # Import every model module here as it is created. A model that is never imported
--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,1 +1,20 @@
 #LLM Configution 
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
+
+
+class LLMConfig:
+    llm = ChatOpenAI(
+        model="gpt-4o-mini",
+        temperature=0.0,
+        streaming=True
+    )
+
+
+class EmbeddingConfig:
+    embedding_model = OpenAIEmbeddings(
+        model="text-embedding-3-small"
+    )
